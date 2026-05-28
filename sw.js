f80890f4b781f74ba2522626efb63a16de9d42aa
xixi270101-netizen <xixi270101-netizen@users.noreply.github.com>
2026-05-28 14:38:44 +0800

    溪溪英语训练计划 - 第一版

---
+++ sw.js
@@ -0,0 +1,19 @@
+// Service Worker for 溪溪英语
+const CACHE = 'xixi-english-v1';
+const FILES = ['index.html', 'manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+      if (res.ok) {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+      }
+      return res;
+    }))
+  );
+});
